5a37ccaee945ed242dcfdc129cf35cd61a910e0f
Kawan Henrique <kawan.inf@gmail.com>
2023-05-28 16:52:20 -0300

    CHORE: Adicionando algoritimo de conversor na API

--- src/routers/monetario.js
+++ src/routers/monetario.js
@@ -1,10 +1,11 @@
 const express = require('express')
 const calcula = require('../utils/conversor')
+const calcular = require('../utils/conversor')
 const router = express.Router()
 
 router.get('/monetario', (req, res) => {
     
-    res.send({Value: "Teste"})
+    res.send({Value: calcular(req.body.valor)})
 })
 
 module.exports = router
